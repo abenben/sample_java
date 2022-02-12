d508e5b8c88a0a7b584954f35b9d339959f97a3e
abenben <abenbenben@gmail.com>
2022-02-12 21:14:10 +0900

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+    サンプルファイル
+ */
+
+/**
+    サンプルクラス
+ */
+public class HelloWorld {
+
+    /**
+     * メイン処理
+     *
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
